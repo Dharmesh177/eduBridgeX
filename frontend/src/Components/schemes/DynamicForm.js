62c5978b545e915651be64f31e1b3c1c6ab87b50
Neel <neelpatel3579@gmail.com>
2023-12-10 20:34:34 +0530

    create dynamic form

--- frontend/src/Components/schemes/DynamicForm.js
+++ frontend/src/Components/schemes/DynamicForm.js
@@ -1,30 +1,324 @@
 import React, { useState } from "react";
 import "./form.css";
 const DynamicForm = () => {
+  const [formData, setFormData] = useState({
+    // Initialize your form fields here
+    scheme_name: "",
+    org_name: "",
+    ref: "",
+    detail: "",
+    benefits: "",
+  });
+
+  const [documents, setdocument] = useState({
+    documents: [],
+  });
+  const [eligibility, seteligibility] = useState({
+    eligibility: [],
+  });
+  const [application_step, setsteps] = useState({
+    staps:[],
+  })
+  const [questions, setquestions] = useState({
+    questions:[]
+  })
+  const [answers, setanswers] = useState({
+    answers:[]
+  })
+
+  const application_step_change_event = (e) => {
+    var main_div = document.getElementById("Application Process");
+    const divs = main_div.getElementsByClassName("text-left");
+    const dataArray = [];
+
+    for (let i = 0; i < divs.length; i++) {
+      const div = divs[i];
+      const t = div.getElementsByTagName("input");
+      const data = t[0].value;
+      dataArray.push(data);
+      t.value = documents[getIndex2(e)];
+      // setdocumentvalue(t)
+      // console.log(documents)
+    }
+
+    setsteps(dataArray);
+    // setdocumentvalue(e.target); // Update the state with the input value
+  };
+  const eligibilityChangeIvent = (e) => {
+    var main_div = document.getElementById("eligibility");
+    const divs = main_div.getElementsByClassName("text-left");
+    const dataArray = [];
+
+    for (let i = 0; i < divs.length; i++) {
+      const div = divs[i];
+      const t = div.getElementsByTagName("input");
+      const data = t[0].value;
+      dataArray.push(data);
+      t.value = documents[getIndex1(e)];
+      // setdocumentvalue(t)
+      // console.log(documents)
+    }
+
+    seteligibility(dataArray);
+    // setdocumentvalue(e.target); // Update the state with the input value
+  };
+  const questionChangeIvent = (e) => {
+    var main_div = document.getElementById("FAQs");
+    const divs = main_div.getElementsByClassName("text-left");
+    const dataArray = [];
+
+    for (let i = 0; i < divs.length; i++) {
+      const div = divs[i];
+      const t = div.getElementsByTagName("input");
+      const data = t[0].value;
+      dataArray.push(data);
+      t.value = questions[getIndex3(e)];
+      // setdocumentvalue(t)
+      // console.log(documents)
+    }
+
+    setquestions(dataArray);
+    // setdocumentvalue(e.target); // Update the state with the input value
+  };
+  const answerChangeIvent = (e) => {
+    var main_div = document.getElementById("FAQs");
+    const divs = main_div.getElementsByClassName("text-left");
+    const dataArray = [];
+
+    for (let i = 0; i < divs.length; i++) {
+      const div = divs[i];
+      const t = div.getElementsByTagName("input");
+      const data = t[1].value;
+      dataArray.push(data);
+      t.value = questions[getIndex3(e)];
+      // setdocumentvalue(t)
+      // console.log(documents)
+    }
+
+    setanswers(dataArray);
+    // setdocumentvalue(e.target); // Update the state with the input value
+  };
+
+  const documentChangeIvent = (e) => {
+    var main_div = document.getElementById("Documents");
+    const divs = main_div.getElementsByClassName("text-left");
+    const dataArray = [];
+
+    for (let i = 0; i < divs.length; i++) {
+      const div = divs[i];
+      const t = div.getElementsByTagName("input");
+      const data = t[0].value;
+      dataArray.push(data);
+      t.value = documents[getIndex2(e)];
+      // setdocumentvalue(t)
+      // console.log(documents)
+    }
+
+    setdocument(dataArray);
+    // setdocumentvalue(e.target); // Update the state with the input value
+  };
+
+  const getIndex = (e) => {
+    var parentDiv = document.getElementById("Documents");
+    var index = Array.prototype.indexOf.call(parentDiv.children, e.target);
+    return index;
+  };
+  const getIndex1 = (e) => {
+    var parentDiv = document.getElementById("eligibility");
+    var index = Array.prototype.indexOf.call(parentDiv.children, e.target);
+    return index;
+  };
+  const getIndex2 = (e) => {
+    var parentDiv = document.getElementById("Application Process");
+    var index = Array.prototype.indexOf.call(parentDiv.children, e.target);
+    return index;
+  };
+  const getIndex3 = (e) => {
+    var parentDiv = document.getElementById("FAQs");
+    var index = Array.prototype.indexOf.call(parentDiv.children, e.target);
+    return index;
+  };
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Call the function to send data to the backend
+    var data = {
+      'questions':questions,
+      'answers':answers,
+      'eligibility':eligibility,
+      'documents':documents,
+      'application step':application_step
+    }
+    var main_data = { ...data, ...formData };
+    console.log(main_data);
+    
+  };
+  function add_documents() {
+    var newEligibilityDiv = document.createElement("div");
+    newEligibilityDiv.className = "text-left";
+    newEligibilityDiv.innerHTML = `
+      <input
+        style="
+          padding: 0.75rem;
+          border-radius: 0.5rem;
+          border-width: 1px;
+          border-color: #9CA3AF;
+          width: 100%;
+          margin-top:5px
+        "
+        placeholder="Documents"
+        
+        type="text"
+        value = ""
+        
+      />
+    `;
+
+    // newEligibilityDiv.value = documents[getIndex(newEligibilityDiv)]
+    var par = document.getElementById("Documents");
+    par.appendChild(newEligibilityDiv);
+    var input_tag = newEligibilityDiv.getElementsByTagName("input");
+    var t = documents[getIndex(newEligibilityDiv)];
+    if (t) {
+      input_tag[0].value = t;
+    }
+    // Add event listener for the 'change' event
+    input_tag[0].addEventListener("change", documentChangeIvent);
+  }
+  function add_eligibility_div() {
+    var newEligibilityDiv = document.createElement("div");
+    newEligibilityDiv.className = "text-left";
+    newEligibilityDiv.innerHTML = `
+      <input
+        style="
+          padding: 0.75rem;
+          border-radius: 0.5rem;
+          border-width: 1px;
+          border-color: #9CA3AF;
+          width: 100%;
+          margin-top:5px
+        "
+        placeholder="Eligibility"
+        type=""
+      />
+    `;
+
+    var par = document.getElementById("eligibility");
+    par.appendChild(newEligibilityDiv);
+    var input_tag = newEligibilityDiv.getElementsByTagName("input");
+    var t = documents[getIndex1(newEligibilityDiv)];
+    if (t) {
+      input_tag[0].value = t;
+    }
+    // Add event listener for the 'change' event
+    input_tag[0].addEventListener("change", eligibilityChangeIvent);
+  }
+  function add_process_steps() {
+    var newEligibilityDiv = document.createElement("div");
+    newEligibilityDiv.className = "text-left";
+    newEligibilityDiv.innerHTML = `
+      <input
+        style="
+          padding: 0.75rem;
+          border-radius: 0.5rem;
+          border-width: 1px;
+          border-color: #9CA3AF;
+          width: 100%;
+          margin-top:5px
+        "
+        placeholder="Steps"
+        type="text"
+      />
+    `;
   
+    var par = document.getElementById("Application Process");
+    par.appendChild(newEligibilityDiv);
+    var input_tag = newEligibilityDiv.getElementsByTagName("input");
+    var t = documents[getIndex2(newEligibilityDiv)];
+    if (t) {
+      input_tag[0].value = t;
+    }
+    // Add event listener for the 'change' event
+    input_tag[0].addEventListener("change", application_step_change_event);
+  }
+  function add_QA() {
+    var newEligibilityDiv = document.createElement("div");
+    newEligibilityDiv.className = "text-left";
+    newEligibilityDiv.innerHTML = `
+      <input
+        style="
+          padding: 0.75rem;
+          border-radius: 0.5rem;
+          border-width: 1px;
+          border-color: #9CA3AF;
+          width: 100%;
+          margin-top:5px
+        "
+        placeholder="Question"
+        type="text"
+      />
+      <input
+        style="
+          padding: 0.75rem;
+          border-radius: 0.5rem;
+          border-width: 1px;
+          border-color: #9CA3AF;
+          width: 100%;
+          margin-top:5px
+          margin-bottom:10px
+        "
+        placeholder="Answer"
+        type="text"
+      />
+    `;
+  
+   
+  
+    var par = document.getElementById("FAQs");
+    par.appendChild(newEligibilityDiv);
+    var input_tag = newEligibilityDiv.getElementsByTagName("input");
+    var q = answers[getIndex2(newEligibilityDiv)];
+    var a = answers[getIndex2(newEligibilityDiv)];
+
+    if (q) {
+      input_tag[0].value = q;
+      input_tag[1].value = a
+    }
+    // Add event listener for the 'change' event
+    input_tag[0].addEventListener("change", questionChangeIvent);
+    input_tag[1].addEventListener("change", answerChangeIvent);
+  }
   return (
     <div className="rounded-lg bg-white p-8 shadow-lg lg:col-span-3 lg:p-12">
-      <form action="" className="space-y-4">
+      <form action="" className="space-y-4" onSubmit={handleSubmit}>
         <div>
-          <label className="sr-only" for="name">
+          <label className="sr-only" htmlFor="name">
             Name
           </label>
           <input
             className="w-full rounded-lg border-gray-400 border p-3 text-m text-black-600"
             placeholder="Scheme name"
             type="text"
-            id="name"
+            name="scheme_name"
+            value={formData.scheme_name}
+            onChange={handleInputChange}
           />
         </div>
         <div>
-          <label className="sr-only" for="org">
+          <label className="sr-only" htmlFor="org">
             Name
           </label>
           <input
             className="w-full rounded-lg border-gray-400 border p-3 text-m text-black-600"
             placeholder="Organization name"
             type="text"
-            id="org"
+            name="org_name"
+            value={formData.org_name}
+            onChange={handleInputChange}
           />
         </div>
 
@@ -37,7 +331,9 @@ const DynamicForm = () => {
             className="w-full rounded-lg border-gray-200 p-3 text-m border"
             placeholder="Details"
             rows="4"
-            id="Details"
+            value={formData.detail}
+            name="detail"
+            onChange={handleInputChange}
           ></textarea>
         </div>
         <div>
@@ -49,13 +345,16 @@ const DynamicForm = () => {
             className="w-full rounded-lg border-gray-200 p-3 text-m border"
             placeholder="Benefits"
             rows="4"
+            value={formData.benefits}
             id="Benefits"
+            name="benefits"
+            onChange={handleInputChange}
           ></textarea>
         </div>
-        <span class="relative flex justify-center">
-          <div class="absolute inset-x-0 top-1/2 h-px -translate-y-1/2 bg-transparent bg-gradient-to-r from-transparent via-gray-500 to-transparent opacity-75"></div>
+        <span className="relative flex justify-center">
+          <div className="absolute inset-x-0 top-1/2 h-px -translate-y-1/2 bg-transparent bg-gradient-to-r from-transparent via-gray-500 to-transparent opacity-75"></div>
 
-          <span class="relative z-10 bg-white px-6">Eligibility</span>
+          <span className="relative z-10 bg-white px-6">Eligibility</span>
         </span>
         <div id="eligibility">
           <div className="text-left">
@@ -63,6 +362,8 @@ const DynamicForm = () => {
               className="ele"
               placeholder="Eligibility"
               type="text"
+              value={eligibility[getIndex1]}
+              onChange={eligibilityChangeIvent}
             />
           </div>
         </div>
@@ -82,16 +383,20 @@ const DynamicForm = () => {
             Remove Eligibility
           </button>
         </div>
-        <span class="relative flex justify-center">
-          <div class="absolute inset-x-0 top-1/2 h-px -translate-y-1/2 bg-transparent bg-gradient-to-r from-transparent via-gray-500 to-transparent opacity-75"></div>
-          <span class="relative z-10 bg-white px-6">Application Process</span>
+        <span className="relative flex justify-center">
+          <div className="absolute inset-x-0 top-1/2 h-px -translate-y-1/2 bg-transparent bg-gradient-to-r from-transparent via-gray-500 to-transparent opacity-75"></div>
+          <span className="relative z-10 bg-white px-6">
+            Application Process
+          </span>
         </span>
         <div id="Application Process">
           <div className="text-left">
-            <input
+          <input
               className="ele"
-              placeholder="Steps"
+              placeholder="Eligibility"
               type="text"
+              value={application_step[getIndex2]}
+              onChange={application_step_change_event}
             />
           </div>
         </div>
@@ -100,6 +405,7 @@ const DynamicForm = () => {
             type="button"
             className="inline-block w-full rounded-lg bg-black px-4 py-2 font-medium text-white sm:w-auto"
             onClick={add_process_steps}
+            
           >
             ADD Steps
           </button>
@@ -111,10 +417,12 @@ const DynamicForm = () => {
             Remove Steps
           </button>
         </div>
-        <span class="relative flex justify-center">
-          <div class="absolute inset-x-0 top-1/2 h-px -translate-y-1/2 bg-transparent bg-gradient-to-r from-transparent via-gray-500 to-transparent opacity-75"></div>
+        <span className="relative flex justify-center">
+          <div className="absolute inset-x-0 top-1/2 h-px -translate-y-1/2 bg-transparent bg-gradient-to-r from-transparent via-gray-500 to-transparent opacity-75"></div>
 
-          <span class="relative z-10 bg-white px-6">Documents Required</span>
+          <span className="relative z-10 bg-white px-6">
+            Documents Required
+          </span>
         </span>
         <div id="Documents">
           <div className="text-left">
@@ -122,6 +430,8 @@ const DynamicForm = () => {
               className="ele"
               placeholder="Documents Name"
               type="text"
+              value={documents[getIndex]}
+              onChange={documentChangeIvent}
             />
           </div>
         </div>
@@ -141,28 +451,26 @@ const DynamicForm = () => {
             Remove Documents
           </button>
         </div>
-        <span class="relative flex justify-center">
-          <div class="absolute inset-x-0 top-1/2 h-px -translate-y-1/2 bg-transparent bg-gradient-to-r from-transparent via-gray-500 to-transparent opacity-75"></div>
+        <span className="relative flex justify-center">
+          <div className="absolute inset-x-0 top-1/2 h-px -translate-y-1/2 bg-transparent bg-gradient-to-r from-transparent via-gray-500 to-transparent opacity-75"></div>
 
-          <span class="relative z-10 bg-white px-6">FAQs</span>
+          <span className="relative z-10 bg-white px-6">FAQs</span>
         </span>
         <div id="FAQs">
           <div className="text-left">
-          
-
-            <input
+          <input
               className="ele"
-              placeholder="Questions"
+              placeholder="Question"
               type="text"
-              
+              value={questions[getIndex3]}
+              onChange={questionChangeIvent}
             />
-          </div>
-          <div className="text-left mb-4">
-            <input
+          <input
               className="ele"
               placeholder="Answer"
               type="text"
-              
+              value={answers[getIndex3]}
+              onChange={answerChangeIvent}
             />
           </div>
         </div>
@@ -190,14 +498,15 @@ const DynamicForm = () => {
             className="w-full rounded-lg border-gray-400 border p-3 text-m text-black-600"
             placeholder="Reference"
             type="text"
-            id="name"
+            name="ref"
+            value={formData.ref}
+            onChange={handleInputChange}
           />
         </div>
         <div>
-        <button
+          <button
             type="submit"
             className="inline-block w-full rounded-lg bg-black px-4 py-2 font-medium text-white sm:w-auto mt-4"
-            
           >
             SUBMIT
           </button>
@@ -209,27 +518,6 @@ const DynamicForm = () => {
 
 export default DynamicForm;
 
-function add_eligibility_div() {
-  var newEligibilityDiv = document.createElement("div");
-  newEligibilityDiv.className = "text-left";
-  newEligibilityDiv.innerHTML = `
-    <input
-      style="
-        padding: 0.75rem;
-        border-radius: 0.5rem;
-        border-width: 1px;
-        border-color: #9CA3AF;
-        width: 100%;
-        margin-top:5px
-      "
-      placeholder="Eligibility"
-      type="text"
-    />
-  `;
-
-  document.getElementById("eligibility").appendChild(newEligibilityDiv);
-}
-
 function remove_eligibility_div() {
   var parentDiv = document.getElementById("eligibility");
   if (parentDiv.children.length > 0) {
@@ -240,7 +528,6 @@ function remove_eligibility_div() {
     parentDiv.removeChild(lastChild);
   }
 }
-
 
 function add_process_steps() {
   var newEligibilityDiv = document.createElement("div");
@@ -255,7 +542,7 @@ function add_process_steps() {
         width: 100%;
         margin-top:5px
       "
-      placeholder="Document"
+      placeholder="Steps"
       type="text"
     />
   `;
@@ -273,26 +560,7 @@ function remove_process_steps() {
     parentDiv.removeChild(lastChild);
   }
 }
-function add_documents() {
-  var newEligibilityDiv = document.createElement("div");
-  newEligibilityDiv.className = "text-left";
-  newEligibilityDiv.innerHTML = `
-    <input
-      style="
-        padding: 0.75rem;
-        border-radius: 0.5rem;
-        border-width: 1px;
-        border-color: #9CA3AF;
-        width: 100%;
-        margin-top:5px
-      "
-      placeholder="Document"
-      type="text"
-    />
-  `;
 
-  document.getElementById("Documents").appendChild(newEligibilityDiv);
-}
 function remove_documents() {
   var parentDiv = document.getElementById("Documents");
   if (parentDiv.children.length > 0) {
@@ -304,44 +572,7 @@ function remove_documents() {
   }
 }
 
-function add_QA() {
-  var newEligibilityDiv = document.createElement("div");
-  newEligibilityDiv.className = "text-left";
-  newEligibilityDiv.innerHTML = `
-    <input
-      style="
-        padding: 0.75rem;
-        border-radius: 0.5rem;
-        border-width: 1px;
-        border-color: #9CA3AF;
-        width: 100%;
-        margin-top:5px
-      "
-      placeholder="Question"
-      type="text"
-    />
-  `;
 
-  document.getElementById("FAQs").appendChild(newEligibilityDiv);
-  var newEligibilityDiv = document.createElement("div");
-  newEligibilityDiv.className = "text-left mb-4";
-  newEligibilityDiv.innerHTML = `
-    <input
-      style="
-        padding: 0.75rem;
-        border-radius: 0.5rem;
-        border-width: 1px;
-        border-color: #9CA3AF;
-        width: 100%;
-        margin-top:5px
-      "
-      placeholder="Answer"
-      type="text"
-    />
-  `;
-
-  document.getElementById("FAQs").appendChild(newEligibilityDiv);
-}
 function remove_QA() {
   var parentDiv = document.getElementById("FAQs");
   if (parentDiv.children.length > 0) {
