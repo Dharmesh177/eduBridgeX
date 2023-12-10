3fdf0c39457b91529660e91aa2177aaec33ac1b9
deepbhut22 <deeppatel6071@gmail.com>
2023-12-10 21:27:44 +0530

    scholarship schema

---
+++ Backend/models/scholarShipModel.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+
+const faqSchema = new mongoose.Schema({
+    question: {
+        type: [String],
+        required: true,
+    },
+    answer: {
+        type: String,
+        required: true,
+    }
+});
+
+const scholarSchipSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    docRequired: {
+        type: [String],
+        required: true,
+        default: [],
+    },
+    eligibility: {
+        type: [String],
+        required: true,
+        default: [],
+    },
+    benifits: {
+        type: String,
+    },
+    organization: {
+        type: String,
+        required: true,
+    },
+    steps: {
+        type: [String],
+        required: true,
+    },
+    FAQ: {
+        type: [faqSchema],
+    }
+});
+
+module.exports = mongoose.model('scholerShip', scholarSchipSchema);
